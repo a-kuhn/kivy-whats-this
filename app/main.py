291d6b1780f5665f9c0fd2f92faf3d00c3d29652
a-kuhn <alex.kuhn1123@gmail.com>
2024-02-17 14:46:12 -0500

    black formatter enabled

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import kivy
-kivy.require('2.3.0')
+
+kivy.require("2.3.0")
 
 from kivy.app import App
 from kivy.uix.label import Label
@@ -9,9 +10,9 @@ class MyApp(App):
 
     def build(self):
         self.title = "What's This?"
-        return Label(text='Hello world')
+        return Label(text="Hello world")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     MyApp().run()
     print()
